532ed284f00817b48ff87f13c9ad3079dbe606cd
Joseph Mazrimas <email": null>
2016-08-25 18:54:14 -0500

    things

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160825195533) do
 
   # These are extensions that must be enabled in order to support this database
@@ -61,12 +60,6 @@ ActiveRecord::Schema.define(version: 20160825195533) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username",                     null: false
-    t.string "email",           default: "", null: false
-    t.string "password_digest", default: "", null: false
   end
 
   create_table "votes", force: :cascade do |t|
