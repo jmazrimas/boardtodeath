b6ee61def287f2aeecccde307d27e7b3e4c200a8
Dan Park & Gardo Roman <pair+danieljpark.gardoroman@devbootcamp.com>
2016-08-25 15:17:41 -0500

    Modified feature test to pull in games added to profile

--- spec/features/user_can_add_and_remove_games_from_collection_spec.rb
+++ spec/features/user_can_add_and_remove_games_from_collection_spec.rb
@@ -17,27 +17,30 @@ feature "user can add and remove games from collection" do
     click_button 'Log in'
     click_on 'Home'
     # title = page.find('div.single-game a').text
-    click_on 'Test game'
+    # game_link = page.find('div.single-game a').first
+    click_on "Test game"
+    # page.find('a').text =
     click_button 'Add to Collection'
     expect(page).to have_content 'Test game'
 
   end
 
   scenario "user signed in and removes game from collection" do
-    email = "new@user.com"
-    username = "newuser"
-     u1 = User.create(username: username, email: email, password: "password")
-     u2 = User.create(username: "username2", email: "email@email2", password: "password")
-     g1 = Game.create(title: "Test game", description: "Test description", user: u2)
+    email2 = "new2@user.com"
+    username2 = "newuser2"
+     u1 = User.create(username: username2, email: email2, password: "password")
+     u2 = User.create(username: "username3", email: "email@email3", password: "password")
+     g1 = Game.create(title: "Test game2", description: "Test description2", user: u2)
     Ownership.create(owner: u1, owned_game: g1)
     visit "/sessions/new"
 
-    page.fill_in 'Email', with: email
+    page.fill_in 'Email', with: email2
     page.fill_in 'Password', with: 'password'
 
     click_button 'Log in'
-    click_on username.capitalize
-    click_on 'Test game'
+    click_on username2.capitalize
+    save_and_open_page
+    click_on 'Test game2'
     click_button 'Remove from Collection'
     expect(page).to have_button "Add to Collection"
   end
