12b720cd91ffd18adea8f35fc2322bc6967c36eb
jmazrimas <joe.mazrimas@gmail.com>
2016-08-24 19:08:54 -0500

    users can add games from the home page. If not logged in, game not created

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,40 @@
 class GamesController < ApplicationController
 
+  before_action :require_login, only: [:create]
+
   def index
+    @game = Game.new
     @games = Game.all
   end
 
   def show
     @game = Game.find(params[:id])
   end
+
+  def create
+    user = User.find_by_id(session[:user_id])
+    game = user.games.new(game_params)
+
+    if game.save
+      redirect_to '/'
+    else
+      redirect_to '/'
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title)
+  end
+
+ 
+  def require_login
+    unless session[:user_id]
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to '/'
+    end
+  end
+
 
 end
