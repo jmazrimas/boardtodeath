efb1a0e2ca399b6521999cc00ec6cdbab0595411
Joseph Mazrimas <email": null>
2016-08-25 09:11:55 -0500

    vote model and tests working

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
 
   belongs_to :user
+  has_many :votes
 
   has_many :ownerships, foreign_key: :owned_game_id
   has_many :owners, through: :ownerships
---
+++ spec/factories/votes.rb
@@ -0,0 +1,8 @@
+
+FactoryGirl.define do
+  factory :vote do
+    user
+    game
+    value 1
+  end
+end
