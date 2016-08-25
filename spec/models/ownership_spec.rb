fb872aeba5a6d70e144e27fff6d2ac0689f913c2
Dan Park & Gardo Roman <pair+danieljpark.gardoroman@devbootcamp.com>
2016-08-24 20:09:51 -0500

    Add test for ownership model

--- spec/models/ownership_spec.rb
+++ spec/models/ownership_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Ownership, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Ownership do
+  let(:u1) {User.create!(username: "Gardoman" , email: "gardo@email.com", password: "password")}
+  let(:g01) {Game.create!(title: "Monopoly", user_id: u1.id, description: "Game about Money")}
+  let(:ownership) {Ownership.create!(owner: u1, owned_game: g01)}
+
+  describe "game ownership" do
+    it "will show owner" do
+      expect(ownership.owner).to eq(u1)
+    end
+
+    it "will show game owned" do
+      expect(ownership.owned_game).to eq(g01)
+    end
+
+  end
 end
